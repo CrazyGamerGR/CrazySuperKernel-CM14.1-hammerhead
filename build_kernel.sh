9b79698c519001df66d34891751e8c2e0c375dbf
FrancescoCG <frank-97@hotmail.gr>
2016-12-25 23:25:32 +0200

    add build_kernel.sh

---
+++ build_kernel.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# Copyright - CrazyGamerGR
+#     		 ________     __   ___
+#    		/ ______|    |  | /  |
+#    		|  |         |  |/  /
+#   		|  |_____    |      \              
+#   		\_______|    |__|\___|
+##########################################
+export ARCH=arm
+export CROSS_COMPILE=/home/francesco/arm-eabi-4.9-linaro/bin/arm-eabi-
+##########################################
+mkdir output
+##########################################
+make -C $(pwd) O=output cyanogenmod_hammerhead_defconfig 
+make -C $(pwd) O=output
+##########################################
+cp output/arch/arm/boot/Image $(pwd)/arch/arm/boot/zImage
+##########################################
+chmod 777 tools_crazy/dtbToolCM
+##########################################
+tools_crazy/dtbToolCM -2 -o outputd/arch/arm/boot/dt.img -s 2048 -p output/scripts/dtc/ output/arch/arm/boot/
+
